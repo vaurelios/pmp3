3628e65a4baa8e1cc8c1940488113808c8e26ef9
Victor A. Santos <victoraur.santos@gmail.com>
2014-06-02 19:14:01 -0300

    added argparse and ProgressBar support

--- pmp3.py
+++ pmp3.py
@@ -1,18 +1,34 @@
 #!/usr/bin/env python3
 
- 
+
+from argparse import ArgumentParser
 from urllib import request
-from bs4 import BeautifulSoup
 from hashlib import md5
+from math import floor
+
+from bs4 import BeautifulSoup
+from progressbar import ProgressBar
 
 
 def split(input, size):
   return [input[start:start+size] for start in range(0, len(input), size)]
+  
+def show_progress(pbar, dd, dt):
+  if dd >= dt: print('\n')
+  
+  percent = floor((dd / dt) * 100)
+  pbar.update(percent)
 
 
-URL = "http://palcomp3.com/bandapressaohits/"
+parser = ArgumentParser(description='Fetch Music files from Palco MP3.')
+parser.add_argument('url', metavar='URL', type=str,
+                    help='Artist page on Palco MP3 to fetch music files')
+parser.add_argument('--fnmask', type=str,  default='{number}. {title} - {artist}.mp3',
+                    help='File name mask')
 
-req = request.urlopen(URL)
+args = parser.parse_args()
+
+req = request.urlopen(args.url)
 
 html = req.read()
 
@@ -21,8 +37,6 @@ soup = BeautifulSoup(html)
 pl = soup.find(class_="player_playlist")
 
 for music in pl.find_all('li'):
-  fnmask = "{number}. {title} - {artist}.mp3"
-
   s = music['data-servidor']
   a = music['data-arquivo']
   number = int(music['data-i']) + 1
@@ -31,7 +45,20 @@ for music in pl.find_all('li'):
 
   e = split(md5(a.encode()).hexdigest(), 1)
   url = "{0}{1}/{2}/{3}/{4}/{5}\n".format(s, e[0], e[1], e[2], e[3], a)
-  fname = fnmask.format(number=number, title=title, artist=artist)
+  fname = args.fnmask.format(number=number, title=title, artist=artist)
   
   with open(fname, 'wb') as mp3:
-    mp3.write(request.urlopen(url).read())
+    resp = request.urlopen(url)
+    total_size = int(resp.headers['Content-Length'].strip())
+    
+    print(fname + ":\n")
+
+    pbar = ProgressBar().start()
+
+    while True:
+      read = resp.read(8192)
+      if (not read): break
+
+      mp3.write(read)
+      show_progress(pbar, mp3.tell(), total_size)
+      
